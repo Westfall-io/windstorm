398dfb4136c33d686da239fac366a93708eea605
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2023-07-26 22:13:36 -0400

    :sparkles: Initial usage of jinja templates

--- src/windstorm/main.py
+++ src/windstorm/main.py
@@ -7,6 +7,33 @@ Created on Mon Jul 17 23:13:52 2023
 """
 
 import sysml2py
+import astropy.units as u
+
+def template_test(model, template):
+    import re
+    from jinja2 import Template
+    
+    template = Template(template)
+    def custom_function(string, element=None):
+        #print(string)
+        z=re.search(r"\.to\((.*)\)", string)
+        if z is not None:
+            # remove the to statement
+            string = string[:z.start(0)]
+            unit_conv = z.groups()[0]
+            #!TODO Need to add support for imperial units 
+            if unit_conv[:2] == 'u.':
+                module = getattr(u, unit_conv[2:])
+            else:
+                raise ValueError('Needs to start with u.')
+            #print(unit_conv)
+        r = model._get_child(string).get_value()
+        if z is not None:
+            r = r.to(module).value
+        return r
+    
+    print(template.render(custom_function=custom_function))
+    return True
 
 
 def main(template_filepath, model_filepath, auth_token=False):
@@ -40,10 +67,13 @@ def main(template_filepath, model_filepath, auth_token=False):
             return False
 
     a = sysml2py.loads(model)
-    a.dump()
 
-    print(a.dump())
-
+    with open(template_filepath, 'r') as f:
+        b = f.read()
+    f.close()
+    
+    template_test(a, b)
+        
     return True
 
 
