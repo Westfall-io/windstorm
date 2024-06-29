c7970da8e10e77b5891456109989ae182ec3a6a8
chriscox-westfall <chriscox-westfall@users.noreply.github.com>
2024-06-29 02:59:50 +0000

    :robot: Format code with black

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,6 +1,7 @@
 import papermill as pm
 
-#pm.execute_notebook(temp_file,temp_file,kernel_name="sysml")
+# pm.execute_notebook(temp_file,temp_file,kernel_name="sysml")
+
 
 def test_null():
     assert None == None
