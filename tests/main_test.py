2a2242cb957f28bda39e48fca64c9b0761558ef1
chriscox-westfall <chriscox-westfall@users.noreply.github.com>
2024-06-30 01:06:15 +0000

    :robot: Format code with black

--- tests/main_test.py
+++ tests/main_test.py
@@ -9,20 +9,20 @@ def test_1_analysis():
     # Create a notebook with the test sysml
     nb = nbf.v4.new_notebook()
     cells = []
-    for dir_path, dir_names, file_names in os.walk(os.path.join('./tests', test_name)):
-        if '.sysml' in file_names:
+    for dir_path, dir_names, file_names in os.walk(os.path.join("./tests", test_name)):
+        if ".sysml" in file_names:
             cells.append(os.path.join(file_names))
 
-    nb['cells'] = []
+    nb["cells"] = []
     for cell in cells:
-        with open(cell, 'r') as f:
-            nb['cells'].append(nbf.v4.new_code_cell(f.read()))
+        with open(cell, "r") as f:
+            nb["cells"].append(nbf.v4.new_code_cell(f.read()))
 
-    fname = 'test.ipynb'
+    fname = "test.ipynb"
 
-    with open(fname, 'w') as f:
+    with open(fname, "w") as f:
         nbf.write(nb, f)
 
-    pm.execute_notebook(fname,fname,kernel_name="sysml")
+    pm.execute_notebook(fname, fname, kernel_name="sysml")
 
     assert None == None
