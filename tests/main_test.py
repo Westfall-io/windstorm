49d4cb5f55b93abec99a34794536d7b88e0fc668
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-06-29 20:57:28 -0400

    :green_heart: Adding capability to add jupyter notebooks to tests

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,7 +1,26 @@
+import os
+
 import papermill as pm
+import nbformat as nbf
 
-# pm.execute_notebook(temp_file,temp_file,kernel_name="sysml")
+def test_1_analysis():
+    test_name = '1_analysis'
+    # Create a notebook with the test sysml
+    nb = nbf.v4.new_notebook()
+    cells = []
+    pring(os.getcwd())
+    #for dir_path, dir_names, file_names in os.walk(os.path.join('./tests', test_name)):
+    #    if '.sysml' in file_names:
+    #        cells.append(os.path.join(file_names)
 
 
-def test_null():
+    #nb['cells'] = [nbf.v4.new_markdown_cell(text),
+    #               nbf.v4.new_code_cell(code)]
+    #fname = 'test.ipynb'
+
+    #with open(fname, 'w') as f:
+    #    nbf.write(nb, f)
+
+    #pm.execute_notebook(fname,fname,kernel_name="sysml")
+
     assert None == None
