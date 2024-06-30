113269533b0203459510c3986846d7957767fbfa
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-06-29 21:24:43 -0400

    :green_heart: Removing mock generation from test

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,28 +1,2 @@
-import os
-
-import papermill as pm
-import nbformat as nbf
-
-
 def test_1_analysis():
-    test_name = "1_analysis"
-    # Create a notebook with the test sysml
-    nb = nbf.v4.new_notebook()
-    cells = []
-    for dir_path, dir_names, file_names in os.walk(os.path.join("./tests", test_name)):
-        if ".sysml" in file_names:
-            cells.append(os.path.join(file_names))
-
-    nb["cells"] = []
-    for cell in cells:
-        with open(cell, "r") as f:
-            nb["cells"].append(nbf.v4.new_code_cell(f.read()))
-
-    fname = "test.ipynb"
-
-    with open(fname, "w") as f:
-        nbf.write(nb, f)
-
-    pm.execute_notebook(fname, fname, kernel_name="sysml")
-
     assert None == None
