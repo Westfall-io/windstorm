5302a9da7c0d04e90f5d4354139d4a79fa38349e
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-06-30 22:57:00 -0400

    :green_heart: Adding json output as separate file.

--- tests/mocks/make_mocks.py
+++ tests/mocks/make_mocks.py
@@ -1,14 +1,14 @@
 import os
+import json
+import base64
 
 import papermill as pm
 import nbformat as nbf
 
 mocks = {}
 for dir_path, dir_names, file_names in os.walk("./tests/mocks"):
-    print(file_names)
     for file in file_names:
         if ".sysml" in file:
-            print("found")
             if not dir_path in mocks:
                 mocks[dir_path] = []
             mocks[dir_path].append(os.path.join(dir_path, file))
@@ -27,3 +27,27 @@ for test_name in mocks:
         nbf.write(nb, f)
 
     pm.execute_notebook(fname, fname, kernel_name="sysml")
+
+    with open(fname, 'r') as f:
+        # Read the notebook
+        nb = json.loads(f.read())
+        for cell in nb["cells"]:
+            # Check for the SysML cells
+            if cell["metadata"]["kernel"] == "SysML" and cell["cell_type"] == "code":
+                # Find the output which must exist now
+                for out in cell["outputs"]:
+                    # Find the right output to continue
+                    if "data" in out.keys():
+                        if "text/html" in out["data"]:
+                            html = out["data"]["text/html"][0]
+                            data = json.loads(base64.b64decode(html[html.find('href=')+35:-14]))
+                            with open(fname.replace('.ipynb', '.json')) as g:
+                                print('Creating mock api file: {}'.format(fname.replace('.ipynb', '.json')))
+                                g.write(json.dumps(data))
+                            ## End if valid name, otherwise can't export
+                        ## End for name
+                    ## End check correct data output
+                ## End for each cell output
+            ## End this cell is SysML
+        ## End for each cell
+    ## Close file
