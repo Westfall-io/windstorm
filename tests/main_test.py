9476a184a592cf101b544fc604ac20c27da45bff
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-06-30 23:28:06 -0400

    :green_heart: Adding responses tests

--- tests/main_test.py
+++ tests/main_test.py
@@ -18,10 +18,51 @@ def test_simple():
     )
 
     resp = requests.get("http://twitter.com/api/1/foobar")
-    resp2 = requests.put("http://example.com")
 
     assert resp.json() == {"error": "not found"}
     assert resp.status_code == 404
+
+@responses.activate
+def test2_simple():
+    # Register via 'Response' object
+    rsp1 = responses.Response(
+        method="PUT",
+        url="http://example.com",
+    )
+    responses.add(rsp1)
+    # register via direct arguments
+    responses.add(
+        responses.GET,
+        "http://twitter.com/api/1/foobar",
+        json={"error": "not found"},
+        status=404,
+    )
+
+    resp2 = requests.put("http://example.com")
+
+    assert resp2.status_code == 200
+    assert resp2.request.method == "PUT"
+
+def return_json():
+    return {"error": "not found"}
+
+@responses.activate
+def test2_simple():
+    # Register via 'Response' object
+    rsp1 = responses.Response(
+        method="PUT",
+        url="http://example.com",
+    )
+    responses.add(rsp1)
+    # register via direct arguments
+    responses.add(
+        responses.GET,
+        "http://twitter.com/api/1/foobar",
+        json=return_json(),
+        status=404,
+    )
+
+    resp2 = requests.put("http://example.com")
 
     assert resp2.status_code == 200
     assert resp2.request.method == "PUT"
