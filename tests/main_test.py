1c8ab59ac01e71205515ccd9efd905588442534f
chriscox-westfall <chriscox-westfall@users.noreply.github.com>
2024-07-01 03:49:58 +0000

    :robot: Format code with black

--- tests/main_test.py
+++ tests/main_test.py
@@ -14,7 +14,8 @@ import requests
 def request_callback(request_id):
     print(request_id)
     headers = {"Content-type": "application/json", "Accept": "text/plain"}
-    return (200, headers, json.dumps({"@id": request_id['@id']}))
+    return (200, headers, json.dumps({"@id": request_id["@id"]}))
+
 
 @responses.activate
 def test_simple():
@@ -73,7 +74,9 @@ def test3_simple():
         content_type="application/json",
     )
 
-    resp2 = requests.post("http://twitter.com/api/1/foobar", {'@id': str(uuid_gen.uuid4())})
+    resp2 = requests.post(
+        "http://twitter.com/api/1/foobar", {"@id": str(uuid_gen.uuid4())}
+    )
 
     assert resp2.status_code == 200
     assert resp2.request.method == "POST"
