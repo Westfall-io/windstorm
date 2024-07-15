19ebb51c433d0142c4a609f611dc9f579c573eb9
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-07-14 23:01:52 -0400

    :construction: Adding successful test

--- tests/main_test.py
+++ tests/main_test.py
@@ -14,13 +14,13 @@ project_response = project_response_fn()
 
 
 @responses.activate
-def test_analysis_simple():
+def test_analysis_no_meta_no_action():
+    """ Nothing to say this is a windstorm element """
+
     add_responses(project_response, "1_analysis")
 
-    analysis = ["case1", "case2"]
-    for a in analysis:
         galestorm(
-            a,
+            "case1",
             api="http://sysml2.intercax.com:9000",
             in_directory="./tests/mocks/1_analysis/input",
             out_directory="./tests/mocks/1_analysis/output",
@@ -32,6 +32,40 @@ def test_analysis_simple():
             )
             == True
         )
+
+@responses.activate
+def test_analysis_no_meta():
+    """ Nothing to say this is a windstorm element, but this one is inside
+    of an action """
+    add_responses(project_response, "1_analysis")
+
+        galestorm(
+            "case2",
+            api="http://sysml2.intercax.com:9000",
+            in_directory="./tests/mocks/1_analysis/input",
+            out_directory="./tests/mocks/1_analysis/output",
+        )
+
+        assert (
+            are_dir_trees_equal(
+                "./tests/mocks/1_analysis/input", "./tests/mocks/1_analysis/output"
+            )
+            == True
+        )
+
+@responses.activate
+def test_analysis_success():
+    """ This should succeed and replace a file with 'No' """
+    add_responses(project_response, "1_analysis")
+
+        galestorm(
+            "case3",
+            api="http://sysml2.intercax.com:9000",
+            in_directory="./tests/mocks/1_analysis/input",
+            out_directory="./tests/mocks/1_analysis/output",
+        )
+        with open("./tests/mocks/1_analysis/output/template.txt", 'r') as f:
+            assert f.read() == 'No'
 
 
 ## These are examples from responses. This one does a static response from a
