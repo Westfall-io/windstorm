4ba840ba5e8670c832da4e1fc2e8c15e96ac66b0
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-07-15 00:07:21 -0400

    :construction: Adding in changes from hotfixes.

--- tests/main_test.py
+++ tests/main_test.py
@@ -33,18 +33,10 @@ def test_analysis_no_meta_no_action():
         == True
     )
 
-<<<<<<< HEAD
 @responses.activate
 def test_analysis_no_meta():
     """ Nothing to say this is a windstorm element, but this one is inside
     of an action """
-=======
-
-@responses.activate
-def test_analysis_no_meta():
-    """Nothing to say this is a windstorm element, but this one is inside
-    of an action"""
->>>>>>> refs/remotes/origin/33-add-support-for-testing
     add_responses(project_response, "1_analysis")
 
     galestorm(
@@ -61,16 +53,9 @@ def test_analysis_no_meta():
         == True
     )
 
-<<<<<<< HEAD
 @responses.activate
 def test_analysis_success():
     """ This should succeed and replace a file with 'No' """
-=======
-
-@responses.activate
-def test_analysis_success():
-    """This should succeed and replace a file with 'No'"""
->>>>>>> refs/remotes/origin/33-add-support-for-testing
     add_responses(project_response, "1_analysis")
 
     galestorm(
@@ -79,63 +64,6 @@ def test_analysis_success():
         in_directory="./tests/mocks/1_analysis/input",
         out_directory="./tests/mocks/1_analysis/output",
     )
-<<<<<<< HEAD
-    with open("./tests/mocks/1_analysis/output/template.txt", 'r') as f:
-        assert f.read() == 'No'
 
-
-## These are examples from responses. This one does a static response from a
-## file.
-
-# @responses.activate
-# def test_1_analysis():
-#    # Static response for projects
-#    with open('./tests/mocks/api_projects_response/projects.json', 'r') as f:
-#        project_response = f.read()
-#
-#    responses.add(
-#        responses.GET,
-#        "http://sysml2.intercax.com:9000/projects?page%5Bsize%5D=1",
-#        json=project_response,
-#        status=200,
-#    )
-
-# responses.add(
-# responses.GET,
-# re.compile("http://sysml2.intercax.com:9000/projects/([a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})"),
-# json=project_response,
-# status=200,
-# )
-
-#    assert resp.json() == {"error": "not found"}
-#    assert resp.status_code == 404
-
-
-# def return_json():
-#    return {"error": "not found"}
-
-## This one just uses a function to return a response.
-# @responses.activate
-# def test2_simple():
-# Register via 'Response' object
-#    rsp1 = responses.Response(
-#        method="PUT",
-#        url="http://example.com",
-#    )
-#    responses.add(rsp1)
-#    # register via direct arguments
-#    responses.add(
-#        responses.GET,
-#        "http://twitter.com/api/1/foobar",
-#        json=return_json(),
-#        status=404,
-#    )
-
-#    resp2 = requests.put("http://example.com")
-
-#    assert resp2.status_code == 200
-#    assert resp2.request.method == "PUT"
-=======
     with open("./tests/mocks/1_analysis/output/template.txt", "r") as f:
         assert f.read() == "No"
->>>>>>> refs/remotes/origin/33-add-support-for-testing
