400853ba41f1818af2595eaf5dd6f0648865312b
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-07-14 21:04:07 -0400

    :construction: Adding function tests.

---
+++ tests/support_tests.py
@@ -0,0 +1,22 @@
+import os.path
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../src/windstorm"))
+
+import pytest
+
+import api.functions as apif
+
+def test_validate_query():
+    with pytest.raises(KeyError):
+        apif.validate({})
+
+    with pytest.raises(KeyError):
+        apif.validate({'value':'no'})
+
+    with pytest.raises(KeyError):
+        apif.validate({'value':'no', 'operator':'no'})
+
+def test_build_query():
+    with pytest.raises(IndexError):
+        apif.validate({'value':'no', 'operator':'no', 'property': 'no'})
