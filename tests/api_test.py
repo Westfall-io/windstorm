8a83c2e6c4e008de56cd31f341d54de6d33c491d
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-07-14 20:22:14 -0400

    :construction: Separating tests into multiple files by function.

---
+++ tests/api_test.py
@@ -0,0 +1,69 @@
+import os.path
+import sys.path
+sys.path.append(os.path.join(os.path.dirname(__file__), "../src/windstorm"))
+
+import pytest
+import responses
+
+from windstorm import galestorm
+from common.functions import *
+
+# Get the default project endpoint response.
+project_response = project_response_fn()
+
+@responses.activate
+def test_404_response():
+    with pytest.raises(SystemExit):
+        responses.add(
+            responses.GET,
+            "http://sysml2.intercax.com:9000/projects?page%5Bsize%5D=1",
+            json={},
+            status=404,
+        )
+
+        galestorm("case1", api="http://sysml2.intercax.com:9000")
+
+
+@responses.activate
+def test_500_response():
+    with pytest.raises(NotImplementedError) as e_info:
+        responses.add(
+            responses.GET,
+            "http://sysml2.intercax.com:9000/projects?page%5Bsize%5D=1",
+            json={},
+            status=500,
+        )
+        galestorm("case1", api="http://sysml2.intercax.com:9000")
+
+
+@responses.activate
+def test_wrong_type_response():
+    with pytest.raises(KeyError) as e_info:
+        responses.add(
+            responses.GET,
+            "http://sysml2.intercax.com:9000/projects?page%5Bsize%5D=1",
+            json={},
+            status=200,
+        )
+
+        galestorm("case1", api="http://sysml2.intercax.com:9000")
+
+
+@responses.activate
+def test_no_element_found():
+    with pytest.raises(SystemExit) as e_info:
+        responses.add(
+            responses.GET,
+            "http://sysml2.intercax.com:9000/projects?page%5Bsize%5D=1",
+            json=project_response,
+            status=200,
+        )
+
+        responses.add(
+            responses.POST,
+            "http://sysml2.intercax.com:9000/projects/00270ef6-e518-455a-b59e-324ffeb1c9da/query-results",
+            json=[],
+            status=200,
+        )
+
+        galestorm("case1", api="http://sysml2.intercax.com:9000")
