fb56c94708d20ca61e81ab5a45ef1b42edbbcd63
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-07-16 19:42:40 -0400

    :test_tube: Adding tests for references to see if it fails.

--- tests/main_test.py
+++ tests/main_test.py
@@ -256,3 +256,46 @@ def test_analysis_success_binary_skip():
     with open("./tests/mocks/1_analysis/output/template.txt", "r") as f:
         assert f.read().strip() == "No"
     f.close()
+
+@responses.activate
+def test_analysis_novariables():
+    """This should succeed and replace a file with 'No'"""
+    add_responses(project_response, "1_analysis")
+
+    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
+        f.write("{{ windstorm('deltaT') }}")
+    f.close()
+
+    galestorm(
+        "case7",
+        api="http://sysml2.intercax.com:9000",
+        in_directory="./tests/mocks/1_analysis/input",
+        out_directory="./tests/mocks/1_analysis/output",
+    )
+
+    assert (
+        are_dir_trees_equal(
+            "./tests/mocks/1_analysis/input", "./tests/mocks/1_analysis/output"
+        )
+        == True
+    )
+
+@responses.activate
+def test_analysis_reference():
+    """This should succeed and replace a file with 'No'"""
+    add_responses(project_response, "1_analysis")
+
+    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
+        f.write("{{ windstorm('deltaT') }}")
+    f.close()
+
+    galestorm(
+        "case7",
+        api="http://sysml2.intercax.com:9000",
+        in_directory="./tests/mocks/1_analysis/input",
+        out_directory="./tests/mocks/1_analysis/output",
+    )
+
+    with open("./tests/mocks/1_analysis/output/template.txt", "r") as f:
+        assert f.read().strip() == "4"
+    f.close()
