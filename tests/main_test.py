5be89eca98c3b9e6f26f14053b6ae0530874515e
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-07-17 19:45:54 -0400

    :white_check_mark: Adding a test for boolean

--- tests/main_test.py
+++ tests/main_test.py
@@ -12,12 +12,16 @@ from tests.common.functions import *
 # Get the default project endpoint response.
 project_response = project_response_fn()
 
+def template(text):
+    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
+        f.write(text)
+    f.close()
 
 @responses.activate
 def test_analysis_no_meta_no_action():
     """Nothing to say this is a windstorm element"""
-
     add_responses(project_response, "1_analysis")
+
     try:
         with open("./tests/mocks/1_analysis/input/template.txt", "r") as f:
             with open("./tests/mocks/1_analysis/output/template.txt", "w") as g:
@@ -73,9 +77,7 @@ def test_analysis_success():
     """This should succeed and replace a file with 'No'"""
     add_responses(project_response, "1_analysis")
 
-    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
-        f.write("{{ windstorm('deltaT') }}")
-    f.close()
+    template("{{ windstorm('deltaT') }}")
 
     galestorm(
         "case3",
@@ -93,9 +95,7 @@ def test_analysis_success_debug():
     """This should succeed and replace a file with 'No'"""
     add_responses(project_response, "1_analysis")
 
-    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
-        f.write("{{ windstorm('deltaT') }}")
-    f.close()
+    template("{{ windstorm('deltaT') }}")
 
     galestorm(
         "case3",
@@ -114,9 +114,7 @@ def test_analysis_featurechain():
     """This should succeed and replace a file with 'No'"""
     add_responses(project_response, "1_analysis")
 
-    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
-        f.write("{{ windstorm('deltaT') }}")
-    f.close()
+    template("{{ windstorm('deltaT') }}")
 
     galestorm(
         "case4",
@@ -134,9 +132,7 @@ def test_analysis_featurechain_deeper():
     """This should succeed and replace a file with 'No'"""
     add_responses(project_response, "1_analysis")
 
-    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
-        f.write("{{ windstorm('deltaT') }}")
-    f.close()
+    template("{{ windstorm('deltaT') }}")
 
     galestorm(
         "case5",
@@ -154,9 +150,7 @@ def test_analysis_units():
     """This should succeed and replace a file with 'No'"""
     add_responses(project_response, "1_analysis")
 
-    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
-        f.write("{{ windstorm('deltaT') }}")
-    f.close()
+    template("{{ windstorm('deltaT') }}")
 
     galestorm(
         "case6",
@@ -174,9 +168,7 @@ def test_failed_template():
     """This should succeed and replace a file with 'No'"""
     add_responses(project_response, "1_analysis")
 
-    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
-        f.write("{{ windstorm('deltaE') }}")
-    f.close()
+    template("{{ windstorm('deltaE') }}")
 
     with pytest.raises(SystemExit):
         galestorm(
@@ -192,9 +184,7 @@ def test_failed_template_success():
     """This should succeed and replace a file with 'No'"""
     add_responses(project_response, "1_analysis")
 
-    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
-        f.write("{{ windstorm('deltaE', 1) }}")
-    f.close()
+    template("{{ windstorm('deltaE', 1) }}")
 
     galestorm(
         "case3",
@@ -212,9 +202,7 @@ def test_failed_template_forced_skip():
     """This should succeed and replace a file with 'No'"""
     add_responses(project_response, "1_analysis")
 
-    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
-        f.write("{{ }")
-    f.close()
+    template("{{ }")
 
     galestorm(
         "case3",
@@ -234,9 +222,7 @@ def test_analysis_success_binary_skip():
     """This should succeed and replace a file with 'No'"""
     add_responses(project_response, "1_analysis")
 
-    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
-        f.write("{{ windstorm('deltaT') }}")
-    f.close()
+    template("{{ windstorm('deltaT') }}")
 
     with open("./tests/mocks/1_analysis/input/binary.b", "wb") as f:
         ba = bytearray([123, 3, 255, 0, 100])
@@ -263,9 +249,7 @@ def test_analysis_novariables():
     """This should succeed and replace a file with 'No'"""
     add_responses(project_response, "1_analysis")
 
-    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
-        f.write("{{ windstorm('deltaT') }}")
-    f.close()
+    template("{{ windstorm('deltaT') }}")
 
     with pytest.raises(SystemExit):
         galestorm(
@@ -281,9 +265,7 @@ def test_analysis_reference():
     """This should succeed and replace a file with 4"""
     add_responses(project_response, "1_analysis")
 
-    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
-        f.write("{{ windstorm('deltaT') }}")
-    f.close()
+    template("{{ windstorm('deltaT') }}")
 
     galestorm(
         "case8",
@@ -302,9 +284,7 @@ def test_analysis_list():
     """This should succeed and replace a file with a list"""
     add_responses(project_response, "1_analysis")
 
-    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
-        f.write("{{ windstorm('deltaT') }}")
-    f.close()
+    template("{{ windstorm('deltaT') }}")
 
     galestorm(
         "case9",
@@ -329,9 +309,7 @@ def test_analysis_jinjafor():
     #    f.write(",{{ v }}")
     #    f.write("{% endfor %})")
     # f.close()
-    with open("./tests/mocks/1_analysis/input/template.txt", "w") as f:
-        f.write("Geopoint({{ windstorm('deltaT')|join(',') }})")
-    f.close()
+    template("Geopoint({{ windstorm('deltaT')|join(',') }})")
 
     galestorm(
         "case9",
@@ -348,3 +326,21 @@ def test_analysis_jinjafor():
     # This can cause type problems
     os.remove("./tests/mocks/1_analysis/input/template.txt")
     os.remove("./tests/mocks/1_analysis/output/template.txt")
+
+@responses.activate
+def test_analysis_boolean():
+    """This should succeed and replace a file with a list"""
+    add_responses(project_response, "1_analysis")
+
+    template("{{ windstorm('deltaT') }}")
+
+    galestorm(
+        "case10",
+        api="http://sysml2.intercax.com:9000",
+        in_directory="./tests/mocks/1_analysis/input",
+        out_directory="./tests/mocks/1_analysis/output",
+    )
+
+    with open("./tests/mocks/1_analysis/output/template.txt", "r") as f:
+        assert bool(f.read().strip())
+    f.close()
