334128205e53b9ea2c8ff38dfc1f7a039add09e1
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-07-17 20:18:47 -0400

    :white_check_mark: Adding test for a condition where the excel file cannot be renamed because a file already exists.

--- tests/excel_test.py
+++ tests/excel_test.py
@@ -90,3 +90,51 @@ def test_analysis_xlsx_force_skip():
     os.remove(f2)
 
     assert b
+
+@responses.activate
+def test_analysis_xlsx_file_exists():
+    """This should skip the excel file since it can't be opened and push the
+    file as is."""
+    add_responses(project_response, "1_analysis")
+
+    f1 = "./tests/mocks/1_analysis/input/template.xlsx"
+    f2 = f1.replace("input", "output")
+
+    shutil.copyfile(
+        "./tests/mocks/1_analysis/xlsx_input/template.xlsx",
+        f1,
+    )
+
+    shutil.copyfile(
+        "./tests/mocks/1_analysis/xlsx_input/template.xlsx",
+        f2,
+    )
+
+    # Update the excel file using the long chain version, doesn't really matter
+    galestorm(
+        "case5",
+        api="http://sysml2.intercax.com:9000",
+        in_directory="./tests/mocks/1_analysis/input",
+        out_directory="./tests/mocks/1_analysis/output",
+    )
+
+    shutil.unpack_archive(
+        "./tests/mocks/1_analysis/output/template.xlsx", "./srczip", "zip"
+    )
+    shutil.unpack_archive(
+        "./tests/mocks/1_analysis/xlsx_input/output.xlsx", "./dstzip", "zip"
+    )
+
+    # Ensure that the file is the same bytewise as the expected output
+    b = are_dir_trees_equal(
+        "./srczip",
+        "./dstzip",
+    )
+
+    shutil.rmtree("./srczip")
+    shutil.rmtree("./dstzip")
+    # Remove the template so that other tests don't pick up
+    os.remove("./tests/mocks/1_analysis/input/template.xlsx")
+    os.remove("./tests/mocks/1_analysis/output/template.xlsx")
+
+    assert b
