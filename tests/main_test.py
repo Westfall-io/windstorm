1f82ec59ab7692c7a24eb328dd63906d6f603e47
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-07-18 19:57:39 -0400

    :construction: Fixing issue with removing files in outside of tests

--- tests/main_test.py
+++ tests/main_test.py
@@ -364,7 +364,6 @@ def test_analysis_multiple_inputs():
     with open("./tests/mocks/1_analysis/output/template.txt", "r") as f:
         assert f.read().strip() == 'True|ABC|1|4|1.1'
 
-
-# This can cause type problems
-os.remove("./tests/mocks/1_analysis/input/template.txt")
-os.remove("./tests/mocks/1_analysis/output/template.txt")
+    # This can cause type problems
+    os.remove("./tests/mocks/1_analysis/input/template.txt")
+    os.remove("./tests/mocks/1_analysis/output/template.txt")
