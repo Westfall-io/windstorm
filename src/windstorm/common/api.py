7d6d2bbad3c053ec8e4fd08b4ded291fa4593f00
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-07-23 19:35:24 -0400

    :construction: Fixing an issue with api not importing correctly

--- src/windstorm/common/api.py
+++ src/windstorm/common/api.py
@@ -9,7 +9,7 @@ import requests
 
 headers = {"Content-type": "application/json", "Accept": "text/plain"}
 
-from windstorm.functions import github_issue_error, is_valid_uuid
+from windstorm.common.functions import github_issue_error, is_valid_uuid
 
 
 def handle_request_response(r):
