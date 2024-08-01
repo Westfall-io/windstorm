756c5d2b1b38132b0c77fb5e3f1c7ee66b18870f
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-07-31 20:32:55 -0400

    :construction: Adding a helm chart test.

--- tests/template_test.py
+++ tests/template_test.py
@@ -19,8 +19,8 @@ def template(text):
 
 
 @responses.activate
-def test_analysis_fail():
-    """This should succeed and replace a file with 'No'"""
+def test_analysis_skip_extra_value():
+    """This should succeed and replace a portion of the file with 'No'"""
     add_responses(project_response, "1_analysis")
 
     template("{{ windstorm('deltaT') }} {{ deltaE }}")
@@ -34,3 +34,22 @@ def test_analysis_fail():
 
     with open("./tests/mocks/1_analysis/output/template.txt", "r") as f:
         assert f.read().strip() == "No {{ deltaE }}"
+
+@responses.activate
+def test_analysis_helm_test():
+    """This should not throw any errors, there's nothing to replace"""
+    add_responses(project_response, "1_analysis")
+
+    galestorm(
+        "case3",
+        api="http://sysml2.intercax.com:9000",
+        in_directory="./tests/minio/",
+        out_directory="./tests/minio_o",
+    )
+
+    assert (
+        are_dir_trees_equal(
+            "./tests/minio/", "./tests/minio_o"
+        )
+        == True
+    )
