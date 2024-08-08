7cfa5f3c4715adb34155e4c34a7b79ea47192862
Christopher Cox <christophercox@MacBook-Pro-5.local>
2024-08-07 20:55:08 -0400

    :construction: More template fixes

--- src/windstorm/common/sysml.py
+++ src/windstorm/common/sysml.py
@@ -333,8 +333,8 @@ def template_files(
                             data = data.replace(match, m2)
                     data = data.replace("`}`", "{\\/\\}")
                     data = data.replace("`", "+{'}")
-                    data = data.replace("\\\\", "&/\\/\\%")
-                    data = data.replace("\\", "&/\\%")
+                    data = data.replace("\\\\", "%+/\\/\\+%")
+                    data = data.replace("\\", "%+/\\+%")
                     # TODO: Handle for loops
                     template = Template(data, keep_trailing_newline=True)
                 except UnicodeDecodeError:
@@ -406,8 +406,8 @@ def template_files(
                         data = data.replace(match, m2)
                     data = data.replace("{\\/\\}", "`}`")
                     data = data.replace("+{'}", "`")
-                    data = data.replace("&/\\/\\%", "\\\\")
-                    data = data.replace("&/\\%","\\")
+                    data = data.replace("%+/\\/\\+%", "\\\\")
+                    data = data.replace("%+/\\+%","\\")
                     f.write(data)
 
     if xlsx["unzip"]:
